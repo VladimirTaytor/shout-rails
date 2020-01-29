3d22ca4900af89f396707d1d934403bb6dd76b5d
Volodymyr Pihol <cetncat@gmail.com>
2020-01-29 21:33:49 +0200

    Implement shouts saving to DB

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,21 @@
+class ShoutsController < ApplicationController
+
+    def create
+        shout = current_user.shouts.create(shouts_params)
+        redirect_to root_path, redirect_options_for(shout)
+    end
+
+    private
+
+    def shouts_params
+        params.require(:shout).permit(:body)
+    end
+
+    def redirect_options_for(shout)
+        if shout.persisted?
+            { notice: "Shouted successfully!" }
+        else
+            { notice: "Shouting failed" }
+        end
+    end
+end
