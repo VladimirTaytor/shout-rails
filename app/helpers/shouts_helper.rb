9db7f48aa410e8b6b0fe9883c884b3bb05780f1e
Volodymyr Pihol <volodymyr.pihol@gmail.com>
2020-02-04 17:57:15 +0200

    Move `#avatar` to `AvatarHelper`
    - Add avatar on users page

--- app/helpers/shouts_helper.rb
+++ app/helpers/shouts_helper.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module ShoutsHelper
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest user.email
-    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
-
-    image_tag gravatar_url
-  end
-
   def like_button(shout)
     return link_to "Unlike",
                    unlike_shout_path(shout),
