fca3a5a5e3aad029dafb09d846c659807c2140a6
Volodymyr Pihol <volodymyr.pihol@gmail.com>
2020-02-06 19:04:12 +0200

    Add searching on file names

--- app/services/search.rb
+++ app/services/search.rb
@@ -1,6 +1,9 @@
 class Search
   def self.in_shouts_for(term)
-    Shout.joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id")
-        .where("text_shouts.body LIKE ?", "%#{term}%")
+    search_query = "%#{term}%"
+    Shout
+        .joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id")
+        .joins("LEFT JOIN photo_shouts ON content_type = 'PhotoShout' AND content_id = photo_shouts.id")
+        .where("text_shouts.body LIKE ? OR photo_shouts.image_file_name LIKE ?", search_query, search_query)
   end
 end
